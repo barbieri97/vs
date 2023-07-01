5c2967480e3716790548e6a9b791830c5aedcc8b
barbieri97 <andrepb636@gmail.com>
2023-07-01 17:19:21 -0300

    add lógica de compra e consumo na api

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for
+
+from random import randrange
 
 app = Flask(__name__)
 
@@ -45,12 +47,37 @@ def token():
 
 @app.route('/api/user')
 def api_user():
-    cookie = request.cookies.get("auth")
     user = request.cookies.get('name')
-    if cookie == '1' and request.args['token'] == '136576357465':
+    if request.args.get('token') == '136576357465':
         with open('licensas.txt', 'r') as f:
             _licensas = f.readlines()
             licensas = [i.strip() for i in _licensas]
         return {'user': user, 'licensas': licensas}
     else:
         return redirect(url_for('index'))
+
+@app.route('/api/buy', methods=['POST'])
+def buy():
+    token = request.form.get('token')
+    if token == '136576357465':
+        new_token = randrange(100000, 999999)
+        with open('licensas.txt', '+a') as file:
+            file.write(f'{str(new_token)}\n')
+        return redirect(url_for('user'))
+    else:
+        return render_template('user.html', error='não foi possivel gerar novo token')
+
+@app.route('/api/pay', methods=['POST'])
+def pay():
+    token = request.form.get('token')
+    if token == '136576357465':
+        new_token = randrange(100000, 999999)
+        with open('licensas.txt', 'r+') as file:
+            token = file.readlines()
+            _ = token.pop()
+            file.truncate(0)
+            file.seek(0)
+            file.writelines(token)
+        return {'status': "succes"}
+    else:
+        return render_template('user.html', error='não foi possivel gerar novo token')
